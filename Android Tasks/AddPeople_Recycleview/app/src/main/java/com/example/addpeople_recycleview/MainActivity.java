cd06a32e58c7eca35d16545b73d02008705ce0ed
tveena-tr <tveenaec>
2020-01-11 00:00:23 +0530

    Android task for add people using RecyclerView

--- Android Tasks/AddPeople_Recycleview/app/src/main/java/com/example/addpeople_recycleview/MainActivity.java
+++ Android Tasks/AddPeople_Recycleview/app/src/main/java/com/example/addpeople_recycleview/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.addpeople_recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    Adapter adapter;
+    ArrayList<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Luke Ray");
+        items.add("Daisy Lake");
+        items.add("Mark Smith");
+        items.add("Daisy Lake");
+        items.add("Mark Smith");
+
+
+
+        recyclerView = findViewById(R.id.RecyclerView1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+
     }
 }
