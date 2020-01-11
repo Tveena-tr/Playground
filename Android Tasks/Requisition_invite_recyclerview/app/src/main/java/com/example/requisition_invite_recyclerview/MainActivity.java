b762e5be1b6faf027076ac585ae215c4c5c58130
tveena-tr <tveenaec>
2020-01-11 22:24:47 +0530

    Android task

--- Android Tasks/Requisition_invite_recyclerview/app/src/main/java/com/example/requisition_invite_recyclerview/MainActivity.java
+++ Android Tasks/Requisition_invite_recyclerview/app/src/main/java/com/example/requisition_invite_recyclerview/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     RecyclerView recyclerView;
 
     Adapter adapter;
@@ -18,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        TextView savedraft = findViewById(R.id.savedraft);
+
+        button.setOnClickListener(this);
+        savedraft.setOnClickListener(this);
 
         items = new ArrayList<>();
         items.add("Luke Ray");
@@ -34,4 +44,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                Toast.makeText(this,"Next",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.savedraft:
+                Toast.makeText(this,"saved",Toast.LENGTH_SHORT).show();
+                break;
+
+
+        }
+
+    }
 }
