1e3d09d0ab738c62951a24af5f0c19c63cb73018
tveena-tr <tveenaec>
2020-01-13 16:36:23 +0530

    Android tasks

--- Android Tasks/jsonexample/app/src/main/java/com/example/jsonexample/MainActivity.java
+++ Android Tasks/jsonexample/app/src/main/java/com/example/jsonexample/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.jsonexample;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.net.sip.SipAudioCall;
-import android.net.sip.SipSession;
-import android.os.Bundle;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Adapter adapter;
     ArrayList<Artical> articals;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +47,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getData() {
-        ProgressDialog progressDialog = new ProgressDialog(this);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading...");
         progressDialog.show();
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
 
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Artical artical = new Artical();
+                        artical.setName(jsonObject.getString("name"));
+                        artical.setDescription(jsonObject.getString("description"));
+                        articals.add(artical);
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(MainActivity.this, "JSON is not valid", Toast.LENGTH_SHORT).show();
+
+                }
+
+                adapter.setData(articals);
+                adapter.notifyDataSetChanged();
+                progressDialog.dismiss();
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this,"Error occurred",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
 
 
 
